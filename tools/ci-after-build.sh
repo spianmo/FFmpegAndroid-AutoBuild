71222b1e42cd0fc668b341f5da6a4bc57a6baa50
Tony Ebichu! <Finger@spianmo.com>
2023-06-21 02:49:59 +0800

    Update ci-after-build.sh

--- tools/ci-after-build.sh
+++ tools/ci-after-build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+echo "bash start"
 ${FORCE_LTO:-false} && LTO_SUFFIX=-lto
 SUFFIX=${FF_VERSION}-${TARGET_OS}
 if [ -n "$COMPILER" ]; then
     SUFFIX+="-${COMPILER}"
 fi
 
+echo "start mv"
+
 SUFFIX+=${LIB_OPT//*-/-}${CONFIG_SUFFIX}${LTO_SUFFIX}
 mv sdk-* ffmpeg-${SUFFIX}
+
+echo "mv end"
+
 export XZ_OPT="--threads=`getconf _NPROCESSORS_ONLN` -9e" # -9e. -8/9 will disable mt?
 tar Jcf ffmpeg-${SUFFIX}{.tar.xz,}
+echo "tar end"
 ls -lh *.xz
 [ "$GITHUB_EVENT_NAME" == "pull_request" ] && exit 0
